95203295f24a71cbb9913a969f68d56c4a960ef3
jhosno <jhosno.dev@gmail.com>
2023-08-17 12:49:36 -0400

    MODELS dogs & temperaments

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -1,9 +1,42 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('dog', {
+  sequelize.define("dog", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    imagen: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    height: {
+      type: DataTypes.INTEGER,
+    },
+    weight: {
+      type: DataTypes.INTEGER,
+    },
+    life_span: {
+      type: DataTypes.STRING,
+    },
+    origin: {
+      type: DataTypes.STRING,
+    },
+  });
+  sequelize.define("temperaments", {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,

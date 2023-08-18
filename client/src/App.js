dd45379c1ed036aff1a1b0ad88c7cb6e5595f461
jhosno <jhosno.dev@gmail.com>
2023-08-18 15:47:33 -0400

    FRONTEND | structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
-import './App.css';
-
+import "./App.css";
+import { Route, Routes, UseNavigate, useLocation } from "react-router-dom";
+import { Home } from "./pages/Home/index";
+import { Landing } from "./pages/Landing/index";
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
       <h1>Henry Dogs</h1>
     </div>
   );

c95b8e1b55fefd6a37583391e9579c2cf69ffe57
jhosno <jhosno.dev@gmail.com>
2023-08-23 14:43:25 -0400

    FRONTEND | LANDING

--- client/src/pages/Landing/index.js
+++ client/src/pages/Landing/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Hero from "../../components/Hero/Hero"
 
 export const Landing = () => {
   return (
-    <div>Land</div>
+    <main>
+      <Hero/>
+    </main>
   )
 }
 

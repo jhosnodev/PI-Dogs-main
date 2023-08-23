44370f6e91411294c75ae68fa8b1c1943bb2b1c2
jhosno <jhosno.dev@gmail.com>
2023-08-23 14:30:46 -0400

    FRONTEND | NAV

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,12 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Nav.css";
+import Logo from '../resource/003-cara-de-perro.svg'
 
 const Nav = () => {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/home">Landing</Link>
-    </div>
+    <nav className="nav___container">
+      <div className="nav___left-inner-container">
+        <Link to="/">
+        <img src={Logo} alt="dogs logo" id="nav___brand"/>
+
+        </Link>
+      </div>
+      <div className="nav___right-inner-container">
+        <Link to="/home">Home</Link>
+        <Link to="/home">Fav</Link>
+        <Link to="/home">Nuevo</Link>
+      </div>
+    </nav>
   );
 };
 

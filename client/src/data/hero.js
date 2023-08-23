74035043f62e25060e5658859cba761a2d871fc2
jhosno <jhosno.dev@gmail.com>
2023-08-23 14:43:47 -0400

    FRONTEND | DATA

---
+++ client/src/data/hero.js
@@ -0,0 +1,10 @@
+const lines = [
+  "Los perros son los mejores amigos. Te darán compañía, amor y apoyo incondicional.",
+  "Los perros pueden ayudarte a ser más activo y saludable. Te obligan a salir a caminar, jugar y hacer ejercicio.",
+  "Los perros pueden reducir el estrés y la ansiedad. Su compañía puede ayudarnos a relajarnos y olvidarnos de nuestros problemas.",
+  "Los perros pueden mejorar nuestro estado de ánimo. Su amor y atención incondicional pueden ayudarnos a sentirnos felices y positivos.",
+  "Los perros pueden enseñarnos responsabilidad. Cuidar de un perro nos obliga a ser responsables de su alimentación, salud y bienestar.",
+  'Si estás buscando una mascota que te proporcione compañía, amor, apoyo, salud y felicidad, un perro es una gran opción. Son animales leales y cariñosos que pueden traer mucha alegría a tu vida.'
+];
+
+export default lines;

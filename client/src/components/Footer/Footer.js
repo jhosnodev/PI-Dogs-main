d3f7d07da094db8386053bd9c8cf38cec7f80e5f
jhosno <jhosno.dev@gmail.com>
2023-08-23 14:30:32 -0400

    FRONTEND | FOOTER

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import './Footer.css'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer className='foote_container'> <p>
+    Individual Proyect | SoyHenry | Made with 🤍 by{" "}
+    <a href="https://jhosno.github.io">Jhosno</a>
+  </p></footer>
   )
 }
 

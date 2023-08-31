3824f133ba2349a085029dbf61d362c159e9354a
jhosno <jhosno.dev@gmail.com>
2023-08-31 14:24:18 -0400

    BACKEND | TRAER PERRITU POR RAZA

--- api/src/controllers/setDog.js
+++ api/src/controllers/setDog.js
@@ -1,16 +1,25 @@
-require("dotenv").config();
-const { BASE_URL } = process.env;
-//console.log(BASE_URL);
+const { Dog } = require("../db");
 
-//! Falta buscar en la DB
+//! Subir relaciones de temp
 const setDog = async (req, res) => {
-  const { breed } = req.params;
+  const data = req.body;
+  console.log(data[0].name);
   try {
-    console.log(`${BASE_URL}/breeds/search?q=${breed}`);
-    const response = await fetch(`${BASE_URL}/search?q=${breed}`);
+    const response = await Dog.findOrCreate({
+      where: {
+        name: data[0].name,
+        imagen: data[0].imagen,
+        height: data[0].height,
+        weight: data[0].weight,
+        bred_for: data[0].bred_for,
+        life_span: data[0].life_span,
+        temperament: data[0].temperament,
+    
+      },
+    });
     const dog = await response.json();
     //console.log(dog);
-    res.status(200).json(dog);
+    res.status(201).json(dog);
   } catch (error) {
     console.error(`Download error: ${error.message}`);
   }

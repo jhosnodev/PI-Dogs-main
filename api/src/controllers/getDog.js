85f226d128b6fd47a69ac7ea5f2279c31dda10bd
jhosno <jhosno.dev@gmail.com>
2023-09-06 21:07:32 -0400

    Update getDog.js

--- api/src/controllers/getDog.js
+++ api/src/controllers/getDog.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
 const { BASE_URL, API_KEY } = process.env;
 //console.log(BASE_URL);
-const axios = require('axios')
+const axios = require("axios");
 
-
-//! Falta buscar en la DB 
+//! Falta buscar en la DB
+//* buscar por ID para detail
 const getDog = async (req, res) => {
-    const {breed} = req.params
+  const { breed } = req.params;
   try {
-    console.log(`${BASE_URL}/breeds/search?q=${breed}`);
-    const {data} = await axios(`${BASE_URL}/search?q=${breed}`); 
+    console.log(`${BASE_URL}/breeds/name?=${breed}`);
+    const { data } = await axios(`${BASE_URL}/search?q=${breed}`);
     //const dog = await response.json();
     console.log(data);
     res.status(200).json(data);

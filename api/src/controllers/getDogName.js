b97fa8b80136938d7658f61dae9760797899b7b8
jhosno <jhosno.dev@gmail.com>
2023-09-06 21:07:38 -0400

    Update getDogName.js

--- api/src/controllers/getDogName.js
+++ api/src/controllers/getDogName.js
@@ -4,14 +4,12 @@ const { BASE_URL } = process.env;
 //! Falta buscar en la DB
 //*Buscar por nombre de raza  */
 const getDogName = async (name) => {
-
   try {
     console.log(`${BASE_URL}/breeds/search?q=${breed}`);
     const response = await fetch(`${BASE_URL}/search?q=${breed}`);
     const dog = await response.json();
     //console.log(dog);
     //return values
-;
   } catch (error) {
     return { error: error };
   }

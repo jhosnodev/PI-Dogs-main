8b48b6bedab48e8f990bf2d4429413fe88a3df59
jhosno <jhosno.dev@gmail.com>
2023-09-06 21:07:41 -0400

    Update setDog.js

--- api/src/controllers/setDog.js
+++ api/src/controllers/setDog.js
@@ -1,28 +1,96 @@
-const { Dog } = require("../db");
+const { Dog, Temperament } = require("../db");
+const axios = require("axios");
 
 //! Subir relaciones de temp
 const setDog = async (req, res) => {
-  const data = req.body;
-  console.log(data[0].name);
-  try {
-    const response = await Dog.findOrCreate({
-      where: {
-        name: data[0].name,
-        imagen: data[0].imagen,
-        height: data[0].height,
-        weight: data[0].weight,
-        bred_for: data[0].bred_for,
-        life_span: data[0].life_span,
-        temperament: data[0].temperament,
-    
-      },
+  const { name, bred_for, height, weight, life_span, temperament } = req.body;
+
+  /* console.log({ name, bred_for, height, weight, life_span, temperament }); */
+
+  if (name && height && weight && life_span && bred_for && temperament) {
+    try {
+      image = await axios.get("https://dog.ceo/api/breeds/image/random");
+    } catch (error) {
+      console.log(error);
+    }
+    /* console.log(name) */
+    const createDog = await Dog.create({
+      name: name,
+      height: height,
+      weight: weight,
+      life_span: life_span,
+      bred_for: bred_for,
+      imagen: image.data.message,
     });
-    const dog = await response.json();
-    //console.log(dog);
-    res.status(201).json(dog);
-  } catch (error) {
-    console.error(`Download error: ${error.message}`);
+    temperament.map(async (temp) => {
+    
+      const findTemp = await Temperament.findAll({
+        where: { id: temp.value },
+      });
+      console.log(temperament[0].value);
+      console.log(findTemp);
+      createDog.addTemperament(findTemp);
+    });
+ /*    console.log(createDog); */
+
+    res.status(200).send(createDog);
+  } else {
+    res.status(404).send("Data needed to proceed is missing");
   }
 };
 
 module.exports = setDog;
+
+const postdog = async (req, res) => {
+  var {
+    // takes these properties to build the new dog
+    name,
+    height_min,
+    height_max,
+    weight_min,
+    weight_max,
+    life_span,
+    temperament,
+    image,
+  } = req.body;
+
+  if (!image) {
+    try {
+      image = await (
+        await axios.get("https://dog.ceo/api/breeds/image/random")
+      ).data.message;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  if (
+    name &&
+    height_min &&
+    height_max &&
+    weight_min &&
+    weight_max &&
+    temperament &&
+    image
+  ) {
+    // takes that data for the new dog
+    const createDog = await Dog.create({
+      name: name,
+      height_min: parseInt(height_min),
+      height_max: parseInt(height_max),
+      weight_min: parseInt(weight_min),
+      weight_max: parseInt(weight_max),
+      life_span: life_span,
+      image: image || "https://dog.ceo/api/breeds/image/random",
+    });
+    temperament.map(async (el) => {
+      const findTemp = await Temperament.findAll({
+        where: { name: el },
+      });
+      createDog.addTemperament(findTemp);
+    });
+    res.status(200).send(createDog);
+  } else {
+    res.status(404).send("Data needed to proceed is missing");
+  }
+};

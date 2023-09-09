65b9fdfabf5633b039bff0e916719ad8fd3259c1
jhosno <jhosno.dev@gmail.com>
2023-09-09 15:08:20 -0400

    Update List.js

--- client/src/components/List/List.js
+++ client/src/components/List/List.js
@@ -4,24 +4,42 @@ import Card from "../Card/Card";
 import { useDispatch, useSelector } from "react-redux";
 import { getDogs } from "../../redux/actions";
 import Pagination from "../Pagition/Pagination";
-import dogs from "../../data/pivot";
-function List() {
+/* import dogs from "../../data/pivot"; */
+
+
+
+function List({ search, setSearch }) {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getDogs());
   }, [dispatch]);
   const allDogs = useSelector((state) => state.dogs);
-/* console.log(allDogs) */
+  const searchResult = useSelector((state) => state.search);
+  /* console.log(allDogs) */
+  
+
+  const handleEaraser = ()=>{
+   
+    dispatch({ type: "RESET_SEARCH" });
+    setSearch('')
+  }
   const [currentPage, setCurrentPage] = useState(1);
   const [dogsPerPage] = useState(8);
-
+  
+  //dogs to list
+  const dogs = searchResult.length ? searchResult : Object.values(allDogs);
+  
+  //pagination settings
   const indexOfLastDog = currentPage * dogsPerPage;
   const indexOfFirstDog = indexOfLastDog - dogsPerPage;
-  const currentDogs = Object.values(allDogs).slice(indexOfFirstDog, indexOfLastDog);
+  const currentDogs = dogs.slice(indexOfFirstDog, indexOfLastDog);
 
   return (
     <div className="list___main">
-      <h3>Listado</h3>
+      <div className="list___header-container">
+        <h3>{searchResult.length > 0 ? `Busqueda de: ${search}` : "Perritus"} </h3>
+        <button onClick={handleEaraser} className={`btn___hightlight btn___primary ${searchResult.length > 0 ? '' : 'hidden'}`}>Limpiar</button>
+      </div>
       <div className="list___container">
         {currentDogs.map((dog) => (
           <Card
@@ -37,7 +55,7 @@ function List() {
         ))}
         <Pagination
           dogsPerPage={dogsPerPage}
-          totalItems={Object.values(allDogs).length}
+          totalItems={dogs.length}
           currentPage={currentPage}
           onPageChange={setCurrentPage}
         />

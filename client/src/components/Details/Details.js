63821a2632bad899876ba3f39d5eeaecff9baad7
jhosno <jhosno.dev@gmail.com>
2023-09-10 12:04:17 -0400

    Redistribuir info

--- client/src/components/Details/Details.js
+++ client/src/components/Details/Details.js
@@ -33,19 +33,15 @@ function Details() {
             : {}
         }
       >
-        <div className="details___dogs-img-gradien"></div>
+        <div className="details___dogs-img-gradient"></div>
       </div>
       <div className="details___dogs-info">
         <Link className="btn___hightlight" to={`dog/edit/${dog?.id}`}>
           Editar
         </Link>
         <button className="details___fav-buttom">❤</button>
-
-        <h2>
-          {dog?.id}
-          {" | "}
-          {dog?.name}
-        </h2>
+        <span className="details___id">ID: {dog?.id}</span>
+        <h2>{dog?.name}</h2>
         <div className="details___tag">
           <h3>Peso (Kg)</h3>
           <span>
@@ -87,6 +83,14 @@ function Details() {
           </ul>
         </div>
       </div>
+      <Link className="btn___comeback  " title="Volver" to='/home'>
+        <svg width="25px" height="25px" viewBox="0 0 2050 2050" fill="#f4f2f2">
+          <path
+
+            d="M1582.2,1488.7a44.9,44.9,0,0,1-36.4-18.5l-75.7-103.9A431.7,431.7,0,0,0,1121.4,1189h-60.1v64c0,59.8-33.5,112.9-87.5,138.6a152.1,152.1,0,0,1-162.7-19.4l-331.5-269a153.5,153.5,0,0,1,0-238.4l331.5-269a152.1,152.1,0,0,1,162.7-19.4c54,25.7,87.5,78.8,87.5,138.6v98.3l161,19.6a460.9,460.9,0,0,1,404.9,457.4v153.4a45,45,0,0,1-45,45Z"
+          />
+        </svg>
+      </Link>
     </article>
   );
 }

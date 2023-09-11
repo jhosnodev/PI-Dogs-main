1c51ccdacaad0cfc7a8f551c43bfc56caa7b4ce6
jhosno <jhosno.dev@gmail.com>
2023-09-11 02:14:44 -0400

    Update Fav.js

--- client/src/components/Fav/Fav.js
+++ client/src/components/Fav/Fav.js
@@ -1,23 +1,17 @@
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 import "./Fav.css";
 import "../List/List.css";
 import Card from "../Card/Card";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 import Pagination from "../Pagition/Pagination";
 /* import dogs from "../../data/pivot"; */
 
-function Fav({ search, setSearch }) {
-  const dispatch = useDispatch();
-
+function Fav() {
   const favs = useSelector((state) => state.favs);
 
   const searchResult = useSelector((state) => state.search);
 
-  const handleEaraser = () => {
-    dispatch({ type: "RESET_SEARCH" });
-    setSearch("");
-  };
   const [currentPage, setCurrentPage] = useState(1);
   const [dogsPerPage] = useState(8);
 
@@ -31,30 +25,34 @@ function Fav({ search, setSearch }) {
 
   return (
     <div className="fav___main">
-      <h3  className="fav___header-container">Tus perritus favoritos</h3>
+      <h3 className="fav___header-container">Tus perritus favoritos</h3>
       <div className="fav___container">
-    
         <div className="fav___container-card">
-          {currentDogs.length === 0 ? 'Guarda algunos perritos para empezar 🐾' : currentDogs.map((dog) => (
-            <Card
-              key={dog.id}
-              id={dog.id}
-              name={dog.name}
-              image={dog.image}
-              temperaments={dog.temperaments}
-              bred_for={dog.bred_for}
-              weight={dog.weight}
-            />
-          ))}
+          {currentDogs.length === 0
+            ? "Guarda algunos perritos para empezar 🐾"
+            : currentDogs.map((dog) => (
+                <Card
+                  key={dog.id}
+                  id={dog.id}
+                  name={dog.name}
+                  image={dog.image}
+                  temperaments={dog.temperaments}
+                  bred_for={dog.bred_for}
+                  weight={dog.weight}
+                />
+              ))}
         </div>
       </div>
-{      currentDogs.length === 0 ? '' : 
-      <Pagination
-        dogsPerPage={dogsPerPage}
-        totalItems={dogs.length}
-        currentPage={currentPage}
-        onPageChange={setCurrentPage}
-      />}
+      {currentDogs.length === 0 ? (
+        ""
+      ) : (
+        <Pagination
+          dogsPerPage={dogsPerPage}
+          totalItems={dogs.length}
+          currentPage={currentPage}
+          onPageChange={setCurrentPage}
+        />
+      )}
     </div>
   );
 }
